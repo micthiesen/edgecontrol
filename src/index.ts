34189df6d36c6ef83cf7a41899797c99d77d9834
Michael Thiesen <michael@understorytech.com>
2024-10-17 18:57:59 -0700

    Added router hostname to env and replaced ssh2 with node-ssh

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
 import { type HttpBindings, serve } from "@hono/node-server";
 import { Hono } from "hono";
-import ssh from "ssh2";
+import { NodeSSH } from "node-ssh";
 import { z } from "zod";
-
-ssh.createAgent(socketPath);
 
 const env = z
   .object({
     PUSHOVER_USER: z.string(),
     PUSHOVER_TOKEN: z.string(),
+    SSH_USERNAME: z.string(),
+    SSH_PASSWORD: z.string(),
+    ROUTER_HOSTNAME: z.string().ip({ version: "v4" }),
   })
   .parse(process.env);
 
 const app = new Hono<{ Bindings: HttpBindings }>();
 
-app.get("/toggle-dns", (c) => {
+app.get("/toggle-dns", async (c) => {
+  const ssh = new NodeSSH();
+  await ssh.connect({
+    hostname: env.ROUTER_HOSTNAME,
+    username: env.SSH_USERNAME,
+    password: env.SSH_PASSWORD,
+  });
+
   const resp = c.text("Hono meets Node.js");
   console.log(`${c.env.incoming.method} ${c.env.incoming.url}`);
   return resp;
