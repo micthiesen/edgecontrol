0325918462517b297f5ed42f09c9287c5a144eef
Michael Thiesen <michael@understorytech.com>
2024-10-17 19:11:27 -0700

    Updated dependencies and improved DNS server extraction logic

--- src/index.ts
+++ src/index.ts
@@ -15,19 +15,45 @@ const env = z
 
 const app = new Hono<{ Bindings: HttpBindings }>();
 
+let timeoutId: NodeJS.Timeout | null = null;
+const DEFERRED_TIMEOUT_MS = 1000 * 5;
+
 app.get("/toggle-dns", async (c) => {
+  if (timeoutId) clearTimeout(timeoutId);
+
+  console.log(`${c.env.incoming.method} ${c.env.incoming.url}`);
+
   const ssh = new NodeSSH();
   await ssh.connect({
-    hostname: env.ROUTER_HOSTNAME,
+    host: env.ROUTER_HOSTNAME,
     username: env.SSH_USERNAME,
     password: env.SSH_PASSWORD,
+    tryKeyboard: false,
   });
 
+  const result = await ssh.execCommand("cat /config/config.boot | grep 'dns-server'");
+  if (result.stderr) return c.text(`Error: ${result.stderr}`);
+
+  timeoutId = setTimeout(async () => {
+    console.log("Restoring original DNS servers...");
+  }, DEFERRED_TIMEOUT_MS);
+
+  const originalDnsServers = extractDnsServers(result.stdout);
+  console.log(`Found existing DNS servers: ${originalDnsServers}`);
+
   const resp = c.text("Hono meets Node.js");
-  console.log(`${c.env.incoming.method} ${c.env.incoming.url}`);
   return resp;
 });
 
 serve({ fetch: app.fetch, port: 5888 }, (info) => {
   console.log(`Listening on http://localhost:${info.port}`);
 });
+
+function extractDnsServers(text: string): [string, string] {
+  const dnsRegex = /dns-server\s+([\d.]+)/g;
+  const matches = Array.from(text.matchAll(dnsRegex)).map((match) => match[1]);
+
+  if (matches.length === 2) return [matches[0], matches[1]];
+
+  throw new Error(`Could not extract DNS servers: ${text}`);
+}
