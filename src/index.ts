f6baedb7bbe53133b0af54f3165f5fb4aef67415
Michael Thiesen <michael@understorytech.com>
2024-10-17 19:20:16 -0700

    Added functionality to temporarily set and restore DNS servers

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { Hono } from "hono";
 import { NodeSSH } from "node-ssh";
 import { z } from "zod";
 
+const ALT_DNS_SERVERS: [string, string] = ["1.1.1.1", "1.0.0.1"];
+
 const env = z
   .object({
     PUSHOVER_USER: z.string(),
@@ -16,13 +18,56 @@ const env = z
 const app = new Hono<{ Bindings: HttpBindings }>();
 
 let timeoutId: NodeJS.Timeout | null = null;
-const DEFERRED_TIMEOUT_MS = 1000 * 5;
+const RESTORE_DELAY = 1000 * 5;
 
 app.get("/toggle-dns", async (c) => {
   if (timeoutId) clearTimeout(timeoutId);
-
   console.log(`${c.env.incoming.method} ${c.env.incoming.url}`);
 
+  let ssh: NodeSSH | undefined;
+  try {
+    ssh = await getSshConnection();
+    const result = await ssh.execCommand("cat /config/config.boot | grep 'dns-server'");
+    if (result.stderr) return c.text(`Error: ${result.stderr}`);
+
+    const originalDnsServers = extractDnsServers(result.stdout);
+    console.log(`Found existing DNS servers: ${originalDnsServers}`);
+
+    console.log("Setting alternative DNS servers...");
+    await setDnsServers(ssh, ALT_DNS_SERVERS);
+
+    timeoutId = setTimeout(genRestoreDnsServers(originalDnsServers), RESTORE_DELAY);
+    console.log(`Scheduled restore in ${RESTORE_DELAY}ms`);
+
+    const resp = c.text("Hono meets Node.js");
+    return resp;
+  } finally {
+    if (ssh) ssh.dispose();
+  }
+});
+
+serve({ fetch: app.fetch, port: 5888 }, (info) => {
+  console.log(`Listening on http://localhost:${info.port}`);
+});
+
+function genRestoreDnsServers(originalDnsServers: [string, string]) {
+  return async () => {
+    console.log("Restoring original DNS servers...");
+    let ssh: NodeSSH | undefined;
+    try {
+      ssh = await getSshConnection();
+      await setDnsServers(ssh, originalDnsServers);
+    } finally {
+      if (ssh) ssh.dispose();
+    }
+  };
+}
+
+async function setDnsServers(ssh: NodeSSH, dns: [string, string]) {
+  console.log(`Setting DNS servers to ${dns}`);
+}
+
+async function getSshConnection(): Promise<NodeSSH> {
   const ssh = new NodeSSH();
   await ssh.connect({
     host: env.ROUTER_HOSTNAME,
@@ -30,24 +75,8 @@ app.get("/toggle-dns", async (c) => {
     password: env.SSH_PASSWORD,
     tryKeyboard: false,
   });
-
-  const result = await ssh.execCommand("cat /config/config.boot | grep 'dns-server'");
-  if (result.stderr) return c.text(`Error: ${result.stderr}`);
-
-  timeoutId = setTimeout(async () => {
-    console.log("Restoring original DNS servers...");
-  }, DEFERRED_TIMEOUT_MS);
-
-  const originalDnsServers = extractDnsServers(result.stdout);
-  console.log(`Found existing DNS servers: ${originalDnsServers}`);
-
-  const resp = c.text("Hono meets Node.js");
-  return resp;
-});
-
-serve({ fetch: app.fetch, port: 5888 }, (info) => {
-  console.log(`Listening on http://localhost:${info.port}`);
-});
+  return ssh;
+}
 
 function extractDnsServers(text: string): [string, string] {
   const dnsRegex = /dns-server\s+([\d.]+)/g;
